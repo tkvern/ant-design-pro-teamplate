5dac0e17476b4928028be340c6f4f61d994ceb28
Vern Zhang <yzhang205@hawk.iit.edu>
2022-06-06 14:08:27 +0800

    chore: update copywrite

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,3 @@
-import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
 import { useIntl } from 'umi';
 
@@ -11,31 +10,7 @@ const Footer: React.FC = () => {
 
   const currentYear = new Date().getFullYear();
 
-  return (
-    <DefaultFooter
-      copyright={`${currentYear} ${defaultMessage}`}
-      links={[
-        {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
-          blankTarget: true,
-        },
-        {
-          key: 'github',
-          title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
-          blankTarget: true,
-        },
-      ]}
-    />
-  );
+  return <DefaultFooter copyright={`${currentYear} ${defaultMessage}`} />;
 };
 
 export default Footer;
